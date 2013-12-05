3f6c63f055e9e1853223d5c1e5549f6bab284f32
Akhil Jain <hp@AKHIL.(none)>
2013-12-05 12:17:46 +0800

    added query 8

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -8,7 +8,7 @@ SELECT item.item_id as a, store.store_id as b, city.city_id as c, city.city_name
 SELECT Store_hold_Item.item_id as items, Store_hold_Item.store_id as stores, city.city_name as city, store.phone as phone FROM Store_hold_Item, store, city WHERE (Store_hold_Item.item_id IN (SELECT Order_contains_items.item_id FROM Order_contains_items WHERE Order_contains_items.order_no = Order.order_no AND Order.Customer_id ='"+a+"')) AND Store_hold_Item.store_id=store.store_id AND store.city_id=city.city_id;
 
 -- 4. Find the headquarter address along with city and state of all stores that hold stocks of an item above a particular level.
-SELECT city.city_name as city, state.state as state, state.headquarter_addr as hq FROM city, state, Store_hold_Item, store WHERE Store_hold_Item.item_id='"+a"' AND Store_hold_Item.quantity_held>'"+b+"' AND Store_hold_Item.store_id=store.store_id AND store.city_id=city.city_id AND city.state=state.state;
+SELECT city.city_name as city, state.state as state, state.headquarter_addr as hq FROM city, state, Store_hold_Item, store WHERE Store_hold_Item.item_id='"+a+"' AND Store_hold_Item.quantity_held>'"+b+"' AND Store_hold_Item.store_id=store.store_id AND store.city_id=city.city_id AND city.state=state.state;
 
 -- 5. For each customer order, show the items ordered along with description, store id and city name and the stores that hold the items.
 SELECT Order_contains_items.item_id as id, item.description as des, store.store_id as store, city.city_name as city FROM Order_contains_items, item, store, city, Store_hold_Item WHERE Order_contains_items.order_no='"+a+"' AND Order_contains_items.item_id=item.item_id AND Store_hold_Item.item_id=item.item_id AND Store_hold_Item.store_id=store.store_id AND store.city_id=city.city_id;
@@ -19,7 +19,8 @@ SELECT city.city_name as city, city.state as state FROM city, customer WHERE Cus
 -- 7. Find the stock level of a particular item in all stores in a particular city.
 SELECT SUM(Store_hold_Item.quantity_held) as stock, city.city_name as city, item.item_id as id, item.description as des FROM Store_hold_Item, city, store WHERE Store_hold_Item.item_id='"+b+"' AND store.store_id=Store_hold_Item.store_id AND city.city_id=store.city_id AND city.city_name='"+a+"';
 
--- 8. Find the items, quantity ordered, customer, store and city of an order.
+-- 8. Find the items, quantity ordered, customer and city of an order.
+SELECT Order_contains_items.item_id as item, item.description as des, Order_contains_items.quantity_ordered as qo, Order.Customer_id as customer, city.city_name as city FROM order, item, Order_contains_items, customer, city WHERE Order_contains_items.order_no='"+a+"' AND Order_contains_items.order_no=order.order_no AND item.item_id=Order_contains_items.item_id AND Order.Customer_id=Customer.Customer_id AND Customer.city_id=city.city_name;
 
 -- 9. Find a list of employee customers with name and discount rate.
 SELECT employee.Employee_customer_name as name, employee.Employee_discount_rate as dis FROM employee ORDER BY employee.Customer_id;;
---
+++ Queries/Query8/Query8_result.jsp
@@ -0,0 +1,53 @@
+<%@ page contentType="text/html; charset=Big5" import="java.util.Date, java.io.*, java.sql.*"%>
+<HTML>
+	<HEAD>
+	<TITLE>
+		Query8_result
+	</TITLE>
+	</HEAD>
+
+	<BODY>
+	<FONT SIZE = 3 COLOR = BLUE face="Arial">
+	<%
+	Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+	Connection con = DriverManager.getConnection("jdbc:odbc:project");
+	Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+							  	
+	String a, b, f1, f2, f3, f4, f5;
+	a = request.getParameter("Query8_select");
+		
+	ResultSet rs = stmt.executeQuery("SELECT Order_contains_items.item_id as item, item.description as des, Order_contains_items.quantity_ordered as qo, Order.Customer_id as customer, city.city_name as city FROM order, item, Order_contains_items, customer, city WHERE Order_contains_items.order_no='"+a+"' AND Order_contains_items.order_no=order.order_no AND item.item_id=Order_contains_items.item_id AND Order.Customer_id=Customer.Customer_id AND Customer.city_id=city.city_name;");
+	%>
+	8. Find the items, quantity ordered, customer and city of an order.
+	<br><br>
+	<TABLE  bgcolor=DodgerBlue>
+		<TR  bgcolor=SkyBlue>
+			<TD><B><font>Item ID</font></B></TD>
+			<TD><B><font>Item Description</font></B></TD>
+			<TD><B><font>Quantity Ordered</font></B></TD>
+			<TD><B><font>Customer ID</font></B></TD>
+			<TD><B><font>City Name</font></B></TD>
+		</TR>	
+		<%	
+	 	while ( rs.next())
+		{
+			f1 = rs.getString("item");
+			f2 = rs.getString("des");
+			f3 = rs.getString("qo");
+			f4 = rs.getString("customer");
+			f5 = rs.getString("city");
+		%>
+		<TR bgcolor=white>
+			<TD><%= f1 %></TD>
+			<TD><%= f2 %></TD>
+			<TD><%= f3 %></TD>
+			<TD><%= f4 %></TD>
+			<TD><%= f5 %></TD>
+		</TR>
+	<%
+	}
+	%>
+	</TABLE>	
+	</FONT>
+	</BODY>
+</HTML>
